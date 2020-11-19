26e4535f2c40011d71657d04337afe3fb2f6957e
Dhruvil <51009478+dhruvilpatel07@users.noreply.github.com>
2020-11-19 15:01:48 -0500

    implemented PayPal and also added e-Receipt

--- GuruLukshmi_Kiosk/App/AppDelegate.swift
+++ GuruLukshmi_Kiosk/App/AppDelegate.swift
@@ -98,3 +98,26 @@ extension Color{
     static let newAddToCartColot = Color("settingListColor")
 }
 
+//Date extension
+extension TimeZone {
+    static let gmt = TimeZone(secondsFromGMT: 0)!
+}
+extension Formatter {
+    static let date = DateFormatter()
+}
+
+extension Date {
+    func localizedDescription(dateStyle: DateFormatter.Style = .medium,
+                              timeStyle: DateFormatter.Style = .medium,
+                           in timeZone : TimeZone = .current,
+                              locale   : Locale = .current) -> String {
+        Formatter.date.locale = locale
+        Formatter.date.timeZone = timeZone
+        Formatter.date.dateStyle = dateStyle
+        Formatter.date.timeStyle = timeStyle
+        return Formatter.date.string(from: self)
+    }
+    var localizedDescription: String { localizedDescription() }
+}
+
+
